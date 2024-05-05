d4d0b8282b075a9e5b53ff0856229635801e418c
Arunkumar032024 <arunkumar032024@gmail.com>
2024-05-05 18:18:24 +0530

    fix bug

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ function setTimer(){
 
 
 
-
-
-
-
 // function to add option in select element 
 function addOption(type, count){
     let opt = document.createElement('option');
     opt.innerHTML = count < 10 ? `0${count}` : count;
-
+}
 // add hour options
 for (let i = 0; i < 12; i++){
     addOption(hourSelect, i);
