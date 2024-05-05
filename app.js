41c10b083a4339e824a098ddb75c3cd1d946b44c
Arunkumar032024 <arunkumar032024@gmail.com>
2024-05-05 18:11:48 +0530

    Add UI

---
+++ app.js
@@ -0,0 +1,31 @@
+// Make variable for buttons 
+let hourSelect = document.querySelector('#hour');
+minSelect = document.querySelector('#min');
+secSelect = document.querySelector('#sec');
+function setTimer(){
+
+}
+
+
+
+
+
+
+
+// function to add option in select element 
+function addOption(type, count){
+    let opt = document.createElement('option');
+    opt.innerHTML = count < 10 ? `0${count}` : count;
+    opt.value = count;
+    type.appendChild(opt);
+}
+
+// add hour options
+for (let i = 0; i < 12; i++){
+    addOption(hourSelect, i);
+}
+// add min and sec options
+for (let i = 0; i < 60; i++){
+    addOption(minSelect, i);
+    addOption(secSelect, i);
+}
